c3b366c472cf7996c4b37cc2044f899b2abba11d
puputtt <putiiwsyalala@upi.edu>
2025-11-25 20:02:44 +0700

    Add files via upload

---
+++ mvp_db.sql
@@ -0,0 +1,116 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 25, 2025 at 02:02 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mvp_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kendaraan`
+--
+
+CREATE TABLE `kendaraan` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `jenis` varchar(50) DEFAULT NULL,
+  `mesin` varchar(100) DEFAULT NULL,
+  `top_speed` int(11) DEFAULT NULL,
+  `tahun_rilis` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `kendaraan`
+--
+
+INSERT INTO `kendaraan` (`id`, `nama`, `jenis`, `mesin`, `top_speed`, `tahun_rilis`) VALUES
+(1, 'Mercedes-AMG F1 W14', 'Mobil', 'Mercedes M14 E Performance', 350, 2023),
+(2, 'Red Bull Racing RB19', 'Mobil', 'Honda RBPTH001', 360, 2023),
+(3, 'Ducati Desmosedici GP23', 'Motor', 'V4 1000cc', 358, 2023),
+(4, 'Yamaha YZR-M1', 'Motor', 'Inline-4 1000cc', 350, 2023),
+(5, 'Ferrari SF-23', 'Mobil', 'Ferrari 066/7', 345, 2023);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembalap`
+--
+
+CREATE TABLE `pembalap` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `tim` varchar(255) NOT NULL,
+  `negara` varchar(255) NOT NULL,
+  `poinMusim` int(11) DEFAULT 0,
+  `jumlahMenang` int(11) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pembalap`
+--
+
+INSERT INTO `pembalap` (`id`, `nama`, `tim`, `negara`, `poinMusim`, `jumlahMenang`) VALUES
+(1, 'Lewis Hamilton', 'Mercedes', 'United Kingdom', 347, 11),
+(2, 'Max Verstappen', 'Red Bull', 'Netherlands', 335, 10),
+(3, 'Valtteri Bottas', 'Mercedes', 'Finland', 203, 2),
+(4, 'Sergio Perez', 'Red Bull', 'Mexico', 190, 1),
+(5, 'Carlos Sainz', 'Ferrari', 'Spain', 150, 0),
+(6, 'Daniel Ricciardo', 'McLaren', 'Australia', 115, 1),
+(7, 'Charles Leclerc', 'Ferrari', 'Monaco', 95, 0),
+(8, 'Lando Norris', 'McLaren', 'United Kingdom', 88, 0),
+(9, 'Pierre Gasly', 'AlphaTauri', 'France', 75, 0),
+(10, 'Fernando Alonso', 'Alpine', 'Spain', 65, 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `kendaraan`
+--
+ALTER TABLE `kendaraan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pembalap`
+--
+ALTER TABLE `pembalap`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `kendaraan`
+--
+ALTER TABLE `kendaraan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `pembalap`
+--
+ALTER TABLE `pembalap`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
